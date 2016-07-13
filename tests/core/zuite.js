ce415423310d548d347541c005cc53cd00a9e757
chesedo <pehshelchesed@gmail.com>
2016-07-13 14:42:27 +0200

    Improve order of tests

--- tests/core/zuite.js
+++ tests/core/zuite.js
@@ -8,8 +8,17 @@ describe( 'Core', function() {
     describe( 'check `_isValidTheme`', function() {
         core._isValidThemeSpec();
     } );
+    describe( 'check `_init` when', function() {
+        core._initSpec();
+    } );
+    describe( 'check `_filterChecks` when', function() {
+        core._filterChecksSpec();
+    } );
     describe( 'check `_blur` when input is', function() {
         core._blurSpec();
+    } );
+    describe( 'check `_performChecks` when', function() {
+        core._performChecksSpec();
     } );
     describe( 'check `setRequired` when called', function() {
         core.setRequiredSpec();
@@ -17,20 +26,11 @@ describe( 'Core', function() {
     describe( 'check `unsetRequired` when called', function() {
         core.unsetRequiredSpec();
     } );
-    describe( 'check `_performChecks` when', function() {
-        core._performChecksSpec();
-    } );
-    describe( 'check `_filterChecks` when', function() {
-        core._filterChecksSpec();
-    } );
     describe( 'check `enableLive` when called', function() {
         core.enableLiveSpec();
     } );
     describe( 'check `disableLive` when called', function() {
         core.disableLiveSpec();
-    } );
-    describe( 'check `_init` when', function() {
-        core._initSpec();
     } );
     describe( 'check `addChecks` when', function() {
         core.addChecksSpec();
